11abe19f28581495f7a4715d3ffbe54f2434beae
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-13 23:06:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -52,7 +52,7 @@ WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT project.category, SUM(pledges.amount)
+"SELECT projects.category, SUM(pledges.amount)
 FROM projects
 INNER JOIN pledges
 ON pledges.project_id = projects.id
